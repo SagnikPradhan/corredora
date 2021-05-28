61f7cb1446a10bf02498ea607cbbaa5bc2c6f402
Sagnik Pradhan <markten6321@gmail.com>
2021-05-28 19:05:34 +0530

    🔨 Further work on example for future
    - Added more log statements
    - Also added start script

--- packages/example/source/index.ts
+++ packages/example/source/index.ts
@@ -2,4 +2,12 @@ import { CorredoraLogger } from "corredora";
 
 const logger = new CorredoraLogger();
 
-logger.log("Hey there!");
+logger.info("Hey there!");
+logger.error(new Error("This is bad code"));
+logger.info(
+  "Loaded 0 modules, but I am going to act I loaded all the modules."
+);
+logger.info({ numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9] });
+
+// @ts-expect-error
+logger.info({ ...JSON.parse(JSON.stringify(process)) });
